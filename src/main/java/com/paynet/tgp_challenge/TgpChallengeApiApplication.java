e0ff37ce3d9654083f1bc3a3d574c3062b888ee8
Shahathir Iskandar <shahathiriskandar42@gmail.com>
2025-03-21 23:13:22 +0800

    first commit

---
+++ src/main/java/com/paynet/tgp_challenge/TgpChallengeApiApplication.java
@@ -0,0 +1,13 @@
+package com.paynet.tgp_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TgpChallengeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TgpChallengeApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/paynet/tgp_challenge/TgpChallengeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paynet.tgp_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TgpChallengeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
